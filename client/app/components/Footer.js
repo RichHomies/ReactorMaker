50d5788a7055aba33a1b883f1e8d3b836e689f61
Zachary Lee <w.zachary.lee@gmail.com>
2015-11-06 19:43:53 -0800

    updated site

---
+++ client/app/components/Footer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-sm-5'>
+              <h3 className='lead'><strong>Information</strong> and <strong>Copyright</strong></h3>
+              <p>Powered by <strong>Node.js</strong>, <strong>MongoDB</strong> and <strong>React</strong> with Flux architecture and server-side rendering.</p>
+            </div>
+            <div className='col-sm-7 hidden-xs'>
+              <h3 className='lead'><strong>Flex</strong> Ooh ooh ooh</h3>
+              <ul className='list-inline'>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
--- client/app/components/Home.js
+++ client/app/components/Home.js
@@ -4,7 +4,15 @@ class Home extends React.Component {
   render() {
     return (
       <div className='container'>
-        Rich Homie Prep
+        <div className='row'>
+          <h1 className="text-center"><strong>We take Javascript beginners to the next level.</strong></h1>
+        </div>
+        <div className='row'>
+          <h2 className='text-center'>ReactorMaker is an 2 week, online program designed to get <strong>you</strong> into Hack Reactor Schools, built by MakerSquare instructional staff.</h2>
+        </div>
+        <div className='row'>
+          <h2 className='text-center'>Upon completion of our program, you will have the skills necessary to ace the intensive technical interview needed to get into Hack Reactor schools.</h2>
+        </div>
       </div>
     );
   }
