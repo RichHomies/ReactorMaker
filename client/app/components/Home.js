4ae073d7f8a12525ae98ab1a1a85f9f6ac5ad824
Zachary Lee <w.zachary.lee@gmail.com>
2015-11-06 21:17:59 -0800

    update

--- client/app/components/Home.js
+++ client/app/components/Home.js
@@ -13,6 +13,19 @@ class Home extends React.Component {
         <div className='row'>
           <h2 className='text-center'>Upon completion of our program, you will have the skills necessary to ace the intensive technical interview needed to get into Hack Reactor schools.</h2>
         </div>
+        <div>
+          <h2 className="text-center">Enter your email for more information.</h2>
+        </div>
+        <div className='row'>
+          <form ref='searchForm' className='navbar-form navbar-center text-center animated'>
+            <div className='input-group input-group-lg'>
+              <input type='text' className='form-control' placeholder='Enter your email' />
+              <span className='input-group-btn'>
+                <button className='btn btn-default'><span className='glyphicon glyphicon-plus'></span></button>
+              </span>
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
