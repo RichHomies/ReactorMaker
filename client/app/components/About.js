6c97f957af166c82c5452444b8fe2099de3bbcf4
Zachary Lee <w.zachary.lee@gmail.com>
2015-11-08 21:25:16 -0800

    update

--- client/app/components/About.js
+++ client/app/components/About.js
@@ -5,7 +5,15 @@ var About = React.createClass({
     return(
       <div className="container home">
         <div className="row">
-          <h3 className="text-center">AYY LMAO</h3>
+          <h1 className="text-center">FAQ</h1>
+        </div>
+        <div className="row">
+          <h2 className="text-center"><strong>Q: What is ReactorMaker?</strong></h2>
+          <h3 className="text-center">A: ReactorMaker is a 2 week online program designed to prepare people for the Javascript-focused Coding Bootcamps' technical interviews.</h3>
+        </div>
+        <div className="row">
+          <h2 className="text-center"><strong>Q: Who created it?</strong></h2>
+          <h3 className="text-center"></h3>
         </div>
       </div>
     )
