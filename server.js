34126642edbd1a49ae91aaaeba29333d36ab148d
Zachary Lee <w.zachary.lee@gmail.com>
2015-11-11 17:53:10 -0800

    added gulp, server works

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var app = express();
 var morgan = require('morgan');
 var parser = require('body-parser');
 var cors = require('cors');
+var path = require('path');
 var cookieParser = require('cookie-parser');
 var routes = require('./server/routes.js');
 var mongoose = require('mongoose');
@@ -11,16 +12,16 @@ var dbUrl = 'mongodb://localhost/richHomiePrep';
 var initServer = function() {
   // attaches all the routes to the server
   routes.setup(app);
-  var port = process.env.PORT || 3001;
+  var port = process.env.PORT || 8080;
   var server = app.listen(port);
   console.log("Express server listening on %d in %s mode", port, app.settings.env)
 }
 
 mongoose.connect(dbUrl);
 app.use(morgan('tiny'));
-app.use(express.static(__dirname + '../client/'));
-app.use(parser.urlencoded({extended: true}));
 app.use(parser.json());
+app.use(parser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'client/public')));
 
 initServer();
 exports.app = app;
